3475d76f1f927c2322df8e0974dd203c3e77342c
pedro01p <tsukki237@gmail.com>
2024-09-04 16:07:51 -0300

    first commit

---
+++ atividade004.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba três notas de um aluno e calcule a média aritmética.
+nota1=float(input("digite a nota 1"))
+nota2=float(input("digite a nota 2"))
+nota3=float(input("digite a nota 3"))
+
+process=(nota1+nota2+nota3)/3
+
+print(f"a media final é {process}")
